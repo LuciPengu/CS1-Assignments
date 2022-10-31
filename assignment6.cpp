c569b131edb0c6638553ba4f2fa0fe31cf3b7ac5
Luci <49926403+LuciPengu@users.noreply.github.com>
2022-10-31 00:19:43 -0400

    Update assignment6.cpp

--- assignment6.cpp
+++ assignment6.cpp
@@ -28,27 +28,27 @@ using namespace std;
 
 int main()
 {
-    double arr[12];
+    double x[12];
 
     for(int i = 1; i <= 12; i++){
         cout << "Enter a value for month " << i << ": ";
-        cin >> arr[i];
-	  while(arr[i] < 0){
+        cin >> x[i];
+	  while(x[i] < 0){
             cout << "Please enter a positive value: ";
-            cin >> arr[i];
+            cin >> x[i];
         }
     }
     
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n = sizeof(x) / sizeof(x[0]);
     double sum;
-    sort(arr, arr + n);
-    sum = accumulate(arr , arr+n , sum);
+    sort(x, x + n);
+    sum = accumulate(x , x+n , sum);
     
     cout << "Total Rainfall: " << sum << endl;
-    cout << "Average Monthly Rainfall: " << (sum/sizeof(arr)) << endl;
-    cout << "Highest Value: " << arr[n-1] << endl;
-    cout << "Lowest Value: " << arr[1] << endl;
-    cout << "Highest Value: " << arr[n-1] << endl;
+    cout << "Average Monthly Rainfall: " << (sum/sizeof(x)) << endl;
+    cout << "Highest Value: " << x[n-1] << endl;
+    cout << "Lowest Value: " << x[1] << endl;
+    cout << "Highest Value: " << x[n-1] << endl;
 
 }
 
@@ -61,28 +61,28 @@ using namespace std;
 int main()
 {
     string salsas[5] = {"mild", "medium", "sweet", "hot", "zesty"};
-    int arr[5];
+    int x[5];
 
     for(int i = 0; i < 5; i++){
         cout << "How many salsas sold that were " << salsas[i] << ": ";
-        cin >> arr[i];
-        while(arr[i] < 0){
+        cin >> x[i];
+        while(x[i] < 0){
             cout << "Please enter a positive value: ";
-            cin >> arr[i];
+            cin >> x[i];
         }
     }
     
-    int n = sizeof(arr) / sizeof(arr[0]);
-    sort(arr, arr + n);
+    int n = sizeof(x) / sizeof(x[0]);
+    sort(x, x + n);
     int sum = 0; 
-    sum = accumulate(arr, arr+n, sum);
+    sum = accumulate(x, x+n, sum);
     
     for(int i = 0; i < 5; i++){
-        cout << salsas[i] << " sold " << arr[i] << " salsas" << endl;
+        cout << salsas[i] << " sold " << x[i] << " salsas" << endl;
     }
     
     cout << "The Total Salsas Sold: " << sum << endl;
-    cout << "Smallest Value: " << arr[0] << endl;
-    cout << "Largest Value: " << arr[n-1] << endl;
+    cout << "Smallest Value: " << x[0] << endl;
+    cout << "Largest Value: " << x[n-1] << endl;
 
 }
